2f591046af4ae4fe866bac2741e1041cd7531f39
Julien Munio <julien.munio@gmail.com>
2021-06-07 16:32:36 +0200

    Add bootstrap lib

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
-import './Navbar.css';
+import "./Navbar.css";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
+      <nav
+        className="navbar"
+        role="navigation"
+        aria-label="main navigation"
+      >
         <div className="navbar-brand">
           <a className="navbar-item" href="/">
             <img
@@ -13,30 +17,30 @@ export default class Navbar extends Component {
               height="28"
               alt="hexal logo"
             />
-            <h4 className="logo"> Jul' Corp</h4>
+            <h4 className="logo text-light font-weight-bold"> Jul' Corp</h4>
           </a>
         </div>
 
         <div id="navbarBasic" className="navbar-menu">
-          <div className="navbar-start">
-            <a href="/" className="navbar-item">
+          <div className="navbar-start d-flex flex-column ">
+            <a href="/" className="navbar-item text-light text-left font-weight-bold text-decoration-none">
               Home
             </a>
-            <a href="/products" className="navbar-item">
+            <a href="/products" className="navbar-item text-light font-weight-bold text-decoration-none">
               Products
             </a>
-            <a href="/admin" className="navbar-item">
+            <a href="/admin" className="navbar-item text-light font-weight-bold text-decoration-none">
               Admin
             </a>
           </div>
 
           <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a href="/register" className="button">
-                  <strong>Sign up</strong>
+            <div className="navbar-item ">
+              <div className="buttons d-flex flex-column">
+                <a href="/register" className="text-light text-decoration-none">
+                  Sign up
                 </a>
-                <a href="/login" className="button">
+                <a href="/login" className="text-light text-decoration-none">
                   Log in
                 </a>
               </div>
